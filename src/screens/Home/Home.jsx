91919dac4ecc54d444cf2f7e66aa174e6eabb072
Md. Wasek Bellah <wasekbellah2311@gmail.com>
2022-07-02 07:25:39 +0600

    featured-list

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,11 +1,16 @@
 import './home.scss';
 import Header from '../common/Header/Header';
 import Navbar from '../common/Header/Navbar';
+import Featured from '../common/Featured/Featured';
 
 const Home = () => {
-	return <div>
+	return <div className='homePage'>
 		<Navbar />
 		<Header />
+
+		<main>
+			<Featured />
+		</main>
 	</div>
 }
 export default Home;
--- src/screens/Hotel/HotelList.jsx
+++ src/screens/Hotel/HotelList.jsx
@@ -1,4 +1,10 @@
+import Header from '../common/Header/Header';
+import Navbar from '../common/Header/Navbar';
+
 const HotelList = () => {
-	return <div>HotelList</div>
+	return <div className='hotelList'>
+		<Navbar />
+		<Header type='list' />
+	</div>
 }
 export default HotelList;
